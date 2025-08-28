2a2aafbac12b32018d6f0756ed91604e5d0c556b
zakhar <zakhar.savchyn.dev@gmail.com>
2025-08-28 22:03:14 +0300

    added main.tf with azurerm provider and backend

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.41.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "matetfstatestorageac52"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
+  }
+}
+
+provider "azurerm" {
+  features {}
+  use_oidc = true
+}
